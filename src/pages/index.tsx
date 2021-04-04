65eac087fc48d83707c75c7c3f157c67d06ee9f5
Renato Bessa <bessache@outlook.com>
2021-04-04 09:48:15 -0300

    commit aula Estilos globais do app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import styles from '../styles/home.module.scss'
+
 import Head from 'next/head';
 
 
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <Head><title>Inicio | ig.news</title></Head>
-      <h1 className={styles.title}>Hello <span>World</span></h1>
+      <h1>Hello World</h1>
     </>
   )
 }
